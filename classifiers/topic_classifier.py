3a440abc9aa89889d7ac4f24ef764d3c5b8f57b0
JSTEP <JStephens3141@gmail.com>
2021-12-27 02:24:05 -0800

    first commit

---
+++ classifiers/topic_classifier.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+
+#Load dataset
+categories = ['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+#print(twenty_train.target_names)
+
+
+#Tokenizing Text
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+#print(count_vect.vocabulary_.get(u'algorithm'))
+
+#TfidfTransformer
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+#print(X_train_Tfidf.shape)
+
+#Create classifier
+clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+
+docs_new = ['God is love', 'OpenGL on the GPU is fast']
+X_new_counts = count_vect.transform(docs_new)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+for doc, category in zip(docs_new, predicted):
+    print('%r => %s' % (doc, twenty_train.target_names[category]))
