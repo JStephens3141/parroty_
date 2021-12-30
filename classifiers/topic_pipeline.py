735ff753697ec5f0c19f76a61ca870d80703de24
JSTEP <JStephens3141@gmail.com>
2021-12-29 22:18:31 -0800

    Initial Commit - Topic pipeline implemented

--- classifiers/topic_pipeline.py
+++ classifiers/topic_pipeline.py
@@ -12,22 +12,22 @@ twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle
 
 #Tokenizing Text
 count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(twenty_train.data)
+##X_train_counts = count_vect.fit_transform(twenty_train.data)
 #print(count_vect.vocabulary_.get(u'algorithm'))
 
 #TfidfTransformer
 tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+##X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 #print(X_train_Tfidf.shape)
 
 #Create classifier
-clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+##clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
 
 docs_new = ['God is love', 'OpenGL on the GPU is fast']
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+##X_new_counts = count_vect.transform(docs_new)
+##X_new_tfidf = tfidf_transformer.transform(X_new_counts)
 
-predicted = clf.predict(X_new_tfidf)
+##predicted = clf.predict(X_new_tfidf)
 
 #for doc, category in zip(docs_new, predicted):
 #    print('%r => %s' % (doc, twenty_train.target_names[category]))
@@ -41,7 +41,7 @@ text_clf = Pipeline([
 
 text_clf.fit(twenty_train.data, twenty_train.target)
 
-predicted_pipeline = text_clf.predict(X_new_tfidf)
+predicted_pipeline = text_clf.predict(docs_new)
 
 
 for doc, category in zip(docs_new, predicted_pipeline):
