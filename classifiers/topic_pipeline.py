5a1b0cbc1ff8821562ffe5f0c1fad5b38bf4c9da
JSTEP <JStephens3141@gmail.com>
2022-01-02 23:54:50 -0800

    Merging model cache

--- classifiers/topic_pipeline.py
+++ classifiers/topic_pipeline.py
@@ -3,46 +3,44 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+import pickle
+from pathlib import Path
+
+environment = 'test'
+home = str(Path.home())
+project_folder = r'\parroty_workspace'
+output_folder = home + project_folder
+pipeline_filename = output_folder + r'\parroty_pipeline.pickle'
 
 #Load dataset
 categories = ['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
 twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
 #print(twenty_train.target_names)
 
+#pipeline cache
+if Path(pipeline_filename).is_file():
+    print('Cached!')
+    with open(pipeline_filename, 'rb') as input:
+        text_clf = pickle.load(input)
+else:
+    #Tokenizing Text
+    count_vect = CountVectorizer()
 
-#Tokenizing Text
-count_vect = CountVectorizer()
-##X_train_counts = count_vect.fit_transform(twenty_train.data)
-#print(count_vect.vocabulary_.get(u'algorithm'))
+    tfidf_transformer = TfidfTransformer()
 
-#TfidfTransformer
-tfidf_transformer = TfidfTransformer()
-##X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-#print(X_train_Tfidf.shape)
+    text_clf = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB())
+    ])
 
-#Create classifier
-##clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+    text_clf.fit(twenty_train.data, twenty_train.target)
+
+    with open(pipeline_filename, 'wb') as output:
+        pickle.dump(text_clf, output)
 
 docs_new = ['God is love', 'OpenGL on the GPU is fast']
-##X_new_counts = count_vect.transform(docs_new)
-##X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-
-##predicted = clf.predict(X_new_tfidf)
-
-#for doc, category in zip(docs_new, predicted):
-#    print('%r => %s' % (doc, twenty_train.target_names[category]))
-
-
-text_clf = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', MultinomialNB())
-])
-
-text_clf.fit(twenty_train.data, twenty_train.target)
-
 predicted_pipeline = text_clf.predict(docs_new)
-
 
 for doc, category in zip(docs_new, predicted_pipeline):
     print('%r => %s' % (doc, twenty_train.target_names[category]))
