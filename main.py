adce0795b07eb61557516ee01bdb283b9a458c41
JSTEP <JStephens3141@gmail.com>
2022-01-03 17:26:20 -0800

    Main pipline build - creating tweet class/object

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ A general purpose classifier pipeline to classify tweet sentiment by 'News Topic
 """
 
 
-import datasources.tweets
+from datasources import tweets
 
-tweet_data = 'data_object'
+tweet_data = tweets()
 
+
+def main():
+    print('Hello')#tweet_data):
 # TODO: Streth Goal - Ask twitter a question/Generate a 'generalized' response
 
 # TODO: Scrape twitter for data
 # TODO: Load data into classifier
 # TODO: Visualize Data
+#    df = tweets()
 
 
+if __name__ == "__main__":
+    main()
