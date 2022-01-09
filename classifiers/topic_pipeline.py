d7356a01d48781a929c0d84ce4854f539a671b90
JSTEP <JStephens3141@gmail.com>
2022-01-09 00:48:10 -0800

    Force Refresh flag, main() thread predictions POC

--- classifiers/topic_pipeline.py
+++ classifiers/topic_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
+#from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
 
@@ -9,29 +9,27 @@ import pickle
 from pathlib import Path
 
 class TopicPipeline():
-    # TODO: Refresh Dataset - Use scrapers/keywords/Social Media Pages/automated sources
-    # TODO: Train Model with ALL categories
+    # TODO: Datasets - Use scrapers/keywords/Social Media Pages/automated sources
 
     def __init__(self):
         environment = 'test'
-        self.home = str(Path.home())
-        self.project_folder = r'\_parroty_workspace'
-        self.output_folder = self.home + self.project_folder
-        self.pipeline_filename = self.output_folder + r'\parroty_pipeline.pickle'
+        home = str(Path.home())
+        project_folder = r'\_parroty_workspace'
+        output_folder = home + project_folder
+        pipeline_filename = output_folder + r'\parroty_pipeline.pickle'
+        self.refresh_model = False
 
         #Load dataset
-        # TODO: add all categories
-        self.categories = ['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
+        #self.categories = ['alt.atheism','soc.religion.christian','comp.graphics', 'sci.med']
         self.twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=11)
-        self.test_data = fetch_20newsgroups(subset='test', shuffle=True, random_state=11)
+        test_data = fetch_20newsgroups(subset='test', shuffle=True, random_state=11)
 
         #pipeline cache
-        # TODO: add check/creation step for self.project_folder
-        # TODO: add a 'Force Refresh' flag that checks if a model should be dumped or kept in cache
+        # TODO: add check/creation step for project_folder
         
-        if Path(self.pipeline_filename).is_file():
+        if Path(pipeline_filename).is_file() and self.refresh_model == False:
             print('Pipeline Loaded!')
-            with open(self.pipeline_filename, 'rb') as input:
+            with open(pipeline_filename, 'rb') as input:
                 self.text_clf = pickle.load(input)
         else:
             #Tokenizing Text
@@ -49,17 +47,18 @@ class TopicPipeline():
 
             self.text_clf.fit(self.twenty_train.data, self.twenty_train.target)
 
-            with open(self.pipeline_filename, 'wb') as output:
+            with open(pipeline_filename, 'wb') as output:
                 pickle.dump(self.text_clf, output)
                 print('Pipeline Cached!')
 
-        self.docs = self.test_data.data#['God is love', 'OpenGL on the GPU is fast']
-        self.predict = self.text_clf.predict(self.docs)
+        #self.docs = test_data.data#['God is love', 'OpenGL on the GPU is fast']
+        #self.predict = self.text_clf.predict(self.docs)
 
         # for doc, category in zip(self.docs, self.predict):
-        #     print('%r => %s' % (doc, self.twenty_train.target_names[category]))
+        #     print('%r => %s' % (doc, twenty_train.target_names[category]))
 
-        print(np.mean(self.predict == self.test_data.target))
+        #Displays the mean correct prediction based on test dataset - will be meaningless for unknown values without manually tagging the data
+        #print(np.mean(self.predict == test_data.target))
 
 if __name__ == '__main__':
     pipeline = TopicPipeline()
