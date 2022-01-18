f6c7aa934d549406ae9fd56746423ed2caf20ac1
jstephens3141 <jstephens3141@gmail.com>
2022-01-18 15:05:29 -0800

    Rolling brand back, adding shell instruction notes

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/data/ data/com.termux / files / usr / bin / bash
+
+#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
+
+#git push https://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@github.com/JStephens3141/parroty_.git
+#git pull
