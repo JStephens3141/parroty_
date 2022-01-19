5da49a721cf9e221e3b15a2616aaef16a8369592
Joseph <JStephens3141@gmail.com>
2022-01-19 14:02:36 -0800

    who needs a pipeline, when you have git

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ rows = [{'tweet': doc,
 def main():
   print('predict: ' + str(predict[0])
   for i in rows:
-    print('rows: ' + i['tweet'])
+    ''#print('rows: ' + i['tweet'])
 
     # TODO: The following dataframe needs to be fed into doc as a string of comma separated values in order to achieve large scale topic classification
     #print(tweet_data.df.data[0])#print(tweet_data.df.tail())
