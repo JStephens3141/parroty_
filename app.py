e7736d075cd5888025740de30178cc7ff5e2a0ff
Joseph <JStephens3141@gmail.com>
2022-01-24 11:31:56 -0800

    Trying flask logger for output to console

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+import logging
+
 import main
 app = Flask(__name__)
 
 @app.route("/")
 def site_index():
     return "<p>Welcome!!</p>"
-    print(2 + 2)
+    app.logger.info('Testing info logger!!')
     main.main()
